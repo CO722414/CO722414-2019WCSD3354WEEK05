da90c38e403a082eeb11cf116ad690edb78d4595
c0722414 <maanraman058@gmail.com>
2019-02-07 14:26:25 -0500

    2:05PM commit

--- CO722414/Program.cs
+++ CO722414/Program.cs
@@ -10,6 +10,33 @@ namespace CO722414
     {
         static void Main(string[] args)
         {
+
+
+
+            var a = new TestQuestion2();
+            a.PlayingWithForLoops();
+        }
+        class TestQuestion2
+        {
+            public int myFavouriteVariables = 0;
+            public void PlayingWithForLoops()
+            {
+                //write a For Loop to Add 10 Numbers
+                for (; MyMethod();)
+                {
+                    if (myFavouriteVariables > 10)
+                    {
+                        Console.WriteLine("i am so out of here!");
+                        break;
+                    }
+                    Console.WriteLine("oh no I have to go through this stupid loop again...");
+                }
+            }
+            public bool MyMethod()
+            {
+                myFavouriteVariables++;
+                return true;
+            }
         }
     }
 }
