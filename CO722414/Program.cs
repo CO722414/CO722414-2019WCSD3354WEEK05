d81e9f13f5ce4b27f1ad739e6c0c48eeee661f74
c0722414 <maanraman058@gmail.com>
2019-02-07 15:36:44 -0500

    3.36pm commit

--- CO722414/Program.cs
+++ CO722414/Program.cs
@@ -18,6 +18,11 @@ namespace CO722414
                 dog_name = name;
                 dog_breed = breed;
             }
+
+            public dog()
+            {
+            }
+
             public string dog_name;
             public string dog_breed;
             public dog next_dog;
@@ -28,12 +33,32 @@ namespace CO722414
         {
             public dog peanut;
             public dog fifi;
-            public dog clarane;
+            public dog clarance;
             public dog roy;
 
             public dog head;
             public dog tail;
             public dog temporary;
+            public void pea()
+
+            {
+                peanut = new dog("peanut", "Bichon");
+                fifi = new dog("fifi", "Poodle");
+                clarance = new dog("Clarence", "German Sheppard");
+                roy = new dog("Roy", "Beagle");
+
+                peanut.prev_dog = null;
+                peanut.next_dog = fifi;
+                fifi.prev_dog = peanut;
+                fifi.next_dog = clarance;
+                clarance.prev_dog = fifi;
+                clarance.next_dog = roy;
+                roy.prev_dog = clarance;
+                roy.next_dog = null;
+                head = peanut;
+                tail = roy;
+
+            }
 
         }
     }
